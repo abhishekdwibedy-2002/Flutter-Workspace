7ab0569ba87df4eeea877555e2a659d1a745e8ec
abhishekdwibedy-2002 <dwibedyabhishek1@gmail.com>
2022-01-30 00:20:10 +0530

    List and Aynomous Button

--- day6/lib/main.dart
+++ day6/lib/main.dart
@@ -1,17 +1,24 @@
-// ignore_for_file: dead_code, deprecated_member_use, prefer_const_literals_to_create_immutables, avoid_print
+// ignore_for_file: dead_code, deprecated_member_use, prefer_const_literals_to_create_immutables, avoid_print, must_be_immutable
 
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
+  var index = 0; // index declaration
   void ansClick() {
-    print('Button Clicked. !');
+    index = index + 1; // index increment
+    print(index);
   }
+
+  var questionList = [
+    'What\'s ur fav color?', //Doing an List box of questions
+    'What\'s ur fav animal?',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,10 +33,11 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
-              const Text('What\'s ur age ?'),
+              Text(questionList
+                  .elementAt(0)), //accesing the question from the list
               ElevatedButton(
                 onPressed:
-                    ansClick, // Naming up the function and calling it everywhere inside the widget
+                    ansClick, //function calling // Naming up the function and calling it everywhere inside the widget
                 child: const Text('28'),
               ),
               ElevatedButton(
